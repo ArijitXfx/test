bf1f22450e85696ce7160d3994152309d0837fbd
ArijitXfx <thearijitxfx@gmail.com>
2021-12-13 14:27:03 +0530

    Subject: Method to build and print list
    Body:
    - New methods added to build and print list which contains 1 to 10
    - Reason is to get a new change to add
    - all methods are static, not a production level code

--- Test.java
+++ Test.java
@@ -1,7 +1,21 @@
 import java.util.*;
 
 public class Test{
+	
+	public static List<Integer> buildList(){
+		List<Integer> list = new ArrayList<>();
+		for(int i=0;i<10;i++)
+			list.add(i);
+		return list;
+	}
+
+	public static void printList(List<Integer> list){
+		for(int i:list)
+			System.out.println(i);
+	}
+
 	public static void main(String[] args){
-		System.out.println("hello world");
+		List<Integer> list = buildList();
+		printList(list);
 	}
 }
